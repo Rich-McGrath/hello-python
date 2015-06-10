ec1567b0038a5ab3a03f71a619011c719d655c4c
Rich-McGrath <richmcgrath@gmail.com>
2015-06-10 16:06:19 -0400

    Added redis CF connection

--- hello.py
+++ hello.py
@@ -1,5 +1,8 @@
 import os
 import uuid
+import urlparse
+import redis
+import json
 from flask import Flask
 app = Flask(__name__)
 my_uuid = str(uuid.uuid1())
@@ -23,5 +26,10 @@ def hello():
 	</html>
 	""".format(COLOR,my_uuid)
 
+
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', port=int(os.getenv('VCAP_APP_PORT', '5000')))
+
+rediscloud_service = json.loads(os.environ['VCAP_SERVICES'])['rediscloud'][0]
+credentials = rediscloud_service['credentials']
+r = redis.Redis(host=credentials['hostname'], port=credentials['port'], password=credentials['password'])
