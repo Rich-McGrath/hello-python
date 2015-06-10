19252d25522e62110aa323e32d37f3eacdc25c7a
Rich-McGrath <richmcgrath@gmail.com>
2015-06-10 16:41:15 -0400

    Changed Redis variables to be defined at begining + set hit counter correctly + print hit counter on screen

--- hello.py
+++ hello.py
@@ -8,32 +8,35 @@ app = Flask(__name__)
 my_uuid = str(uuid.uuid1())
 BLUE = "#0099FF"
 GREEN = "#33CC33"
-
 COLOR = BLUE
+rediscloud_service = json.loads(os.environ['VCAP_SERVICES'])['rediscloud'][0]
+credentials = rediscloud_service['credentials']
+r = redis.Redis(host=credentials['hostname'], port=credentials['port'], password=credentials['password'])
+r.set("hit_counter", 1)
+r.incr("hit_counter")
 
 @app.route('/')
 def hello():
+
 
 	return """
 	<html>
 	<body bgcolor="{}">
 
 	<center><h1><font color="white">Hi, I'm GUID:<br/>
-	{}
+	<center><h1><font color="white">Hit Counter:<br/>
+	{}{}
 	</center>
 
 	</body>
 	</html>
-	""".format(COLOR,my_uuid)
+	""".format(COLOR,my_uuid,r.get("hit_counter"))
+
+
 
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', port=int(os.getenv('VCAP_APP_PORT', '5000')))
 
-rediscloud_service = json.loads(os.environ['VCAP_SERVICES'])['rediscloud'][0]
-credentials = rediscloud_service['credentials']
-r = redis.Redis(host=credentials['hostname'], port=credentials['port'], password=credentials['password'])
-
 r.set("hit_counter", 1)
 r.incr("hit_counter")
-r.get("hit_counter")
