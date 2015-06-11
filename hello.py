7b8f45b0ca50ad905ee8fab7b1c110fb8bb2e493
Rich-McGrath <richmcgrath@gmail.com>
2015-06-11 18:22:27 -0400

    Added new relic startup lines

--- hello.py
+++ hello.py
@@ -3,6 +3,8 @@ import uuid
 import urlparse
 import redis
 import json
+import newrelic.agent
+newrelic.agent.initialize('newrelic.ini')
 from flask import Flask
 app = Flask(__name__)
 my_uuid = str(uuid.uuid1())
